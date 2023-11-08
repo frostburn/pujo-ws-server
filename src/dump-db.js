042dfb05de50f09bd75be706d81ba11273d8a6ec
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-11-08 16:02:08 +0200

    Dump/restore replays in expanded form

--- src/dump-db.js
+++ src/dump-db.js
@@ -1,9 +1,20 @@
 import sql from './db.js';
+import {loadReplay} from './storage.js';
 
 // TODO: Make streaming before there's too much data.
 (async () => {
   const users = await sql`SELECT * FROM users;`;
-  const replays = await sql`SELECT * FROM replays;`;
+  const rows =
+    await sql`SELECT id, private, left_player, right_player FROM replays;`;
+
+  const replays = [];
+  for (const row of rows) {
+    const replay = await loadReplay(row.id);
+    replay.private = row.private;
+    replay.leftPlayer = row.leftPlayer;
+    replay.rightPlayer = row.rightPlayer;
+    replays.push(replay);
+  }
 
   process.stdout.write(JSON.stringify({users, replays}));
   process.stdout.write('\n');
--- src/restore-db.js
+++ src/restore-db.js
@@ -1,18 +1,23 @@
 import {readFileSync} from 'fs';
 import sql from './db.js';
+import {saveReplay} from './storage.js';
 
 // TODO: Make streaming before there's too much data.
 (async () => {
   const data = JSON.parse(readFileSync(process.argv[2]));
 
+  await sql`DELETE FROM replays;`;
   await sql`DELETE FROM users;`;
+
   if (data.users.length) {
     await sql`INSERT INTO users ${sql(data.users)};`;
   }
 
-  await sql`DELETE FROM replays;`;
-  if (data.replays.length) {
-    await sql`INSERT INTO replays ${sql(data.replays)};`;
+  for (const replay of data.replays) {
+    await saveReplay(replay, replay.private, [
+      replay.leftPlayer,
+      replay.rightPlayer,
+    ]);
   }
 
   await sql.end();
